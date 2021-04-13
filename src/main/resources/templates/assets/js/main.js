687838d3d0a79edf87762989d11070d0a72f1250
Domia <39hson@gmail.com>
2021-04-13 19:51:56 +0700

    done switch add-order and add-table

--- src/main/resources/templates/assets/js/main.js
+++ src/main/resources/templates/assets/js/main.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
-  let menuStack = false;
+  let orderStack = false;
+  let tableStack = false;
   $(".table-item").click(function () {
-    if (!menuStack) {
+    if (!orderStack && !tableStack) {
       $(".order-list").addClass("d-block");
-      menuStack = true;
+      orderStack = true;
+    }
+    if (!orderStack && tableStack) {
+      $(".table-add-form").removeClass("d-block");
+      tableStack = false;
+      $(".order-list").addClass("d-block");
+      orderStack = true;
     }
   });
   $(".order-list-cancel-btn").click(function () {
     $(".order-list").removeClass("d-block");
-    menuStack = false;
+    orderStack = false;
+  });
+  $(".table-add-btn").click(function () {
+    if (!tableStack && !orderStack) {
+      $(".table-add-form").addClass("d-block");
+      tableStack = true;
+    }
+    if (!tableStack && orderStack) {
+      $(".order-list").removeClass("d-block");
+      orderStack = false;
+      $(".table-add-form").addClass("d-block");
+      tableStack = true;
+    }
   });
   $(".table-add-cancel-btn").click(function () {
     $(".table-add-form").removeClass("d-block");
-    menuStack = false;
-  });
-  $(".table-add-btn").click(function () {
-    if (!menuStack) {
-      $(".table-add-form").addClass("d-block");
-      menuStack = true;
-    }
+    tableStack = false;
   });
 });
