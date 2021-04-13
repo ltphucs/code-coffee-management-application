98edc59002c4b2042e1db01cd5260c3536e3bb60
Domia <39hson@gmail.com>
2021-04-13 21:23:33 +0700

    test 1

--- src/main/java/com/nobita/demo/controller/api/AccountRestController.java
+++ src/main/java/com/nobita/demo/controller/api/AccountRestController.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//test1
 @RestController
 @RequestMapping(value = "/api/v1/accounts")
 public class AccountRestController {
