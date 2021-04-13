e204a1f2d9c0f75926e9e009dbecb11bc768c221
Domia <39hson@gmail.com>
2021-04-13 19:41:55 +0700

    fix something

--- src/main/resources/templates/assets/js/main.js
+++ src/main/resources/templates/assets/js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
+  let menuStack = false;
   $(".table-item").click(function () {
-    $(".order-list").addClass("d-block");
+    if (!menuStack) {
+      $(".order-list").addClass("d-block");
+      menuStack = true;
+    }
   });
   $(".order-list-cancel-btn").click(function () {
     $(".order-list").removeClass("d-block");
+    menuStack = false;
   });
   $(".table-add-cancel-btn").click(function () {
     $(".table-add-form").removeClass("d-block");
+    menuStack = false;
   });
   $(".table-add-btn").click(function () {
-    $(".table-add-form").addClass("d-block");
+    if (!menuStack) {
+      $(".table-add-form").addClass("d-block");
+      menuStack = true;
+    }
   });
 });
