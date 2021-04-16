20b8da5e9635680f01ed9dc35a1ec6242d4e8429
Domia <39hson@gmail.com>
2021-04-16 10:10:57 +0700

    add product list

---
+++ src/main/resources/static/js/products.js
@@ -0,0 +1,34 @@
+var products = {};
+
+products.initProductTable = function () {
+    console.log("adfs");
+    $("#products-datatables").DataTable({
+        ajax: {
+            url: "http://localhost:8080/api/products/",
+            method: "GET",
+            dataType: "json",
+            dataSrc: ""
+        },
+        columns: [
+            {data: "name", name: "name", title: "Tên sản phẩm", orderable: true},
+            {data: "inventory", name: "inventory", title: "Số lượng trong kho"},
+            {data: "price", name: "price", title: "Giá"},
+            {data: "productLine.name", name: "productLine.name", title: "Dòng sản phẩm"},
+            {data: "productStatus", name: "productStatus", title: "Trạng thái"},
+            {
+                data: "id", name: "action", title: "Chức năng", orderable: false,
+                "render": function (data, type, row, meta) {
+                    var str = "<a href='javascript:;' title='edit product'><i class='fa fa-edit'></i></a> " +
+                        "<a href='javascript:;' title='remove product' ><i class='fa fa-trash'></i></a>"
+                    return str;
+                }
+            },
+        ]
+    });
+
+}
+
+$(document).ready(function () {
+    products.initProductTable();
+
+});
