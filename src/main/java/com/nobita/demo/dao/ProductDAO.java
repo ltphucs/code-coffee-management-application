65b25e07a5b8d74665764b429bfb9c8203181155
Domia <39hson@gmail.com>
2021-04-21 19:12:36 +0700

    done add import-product

--- src/main/java/com/nobita/demo/dao/ProductDAO.java
+++ src/main/java/com/nobita/demo/dao/ProductDAO.java
@@ -23,35 +23,28 @@ public class ProductDAO implements BaseDAO<Product> {
     @Override
     public Product findByID(Long id) {
         String sql = "select p.* ,pl.name as name_productline from product p left join productline pl on pl.id =p.id_productline where p.id=?";
-        Object [] values ={id};
-        return jdbcTemplate.queryForObject(sql, new ProductRowMapper(),values);
+        Object[] values = {id};
+        return jdbcTemplate.queryForObject(sql, new ProductRowMapper(), values);
     }
-
-//    @Override
-//    public Product findByProductLine(Long productLineId) {
-//        String sql = "SELECT * FROM product WHERE id_productline = 6 or id_productline = 7";
-//        Object [] values ={productLineId};
-//        return jdbcTemplate.queryForObject(sql, new ProductRowMapper(),values);
-//    }
 
     @Override
     public boolean save(Product product) {
-        String sql ="insert into product(name,inventory,price,id_productline,image,status) values(?,?,?,?,?,?)";
-        Object [] values={product.getName(),product.getInventory(),product.getPrice(),product.getProductLine().getId(),product.getImage(),product.getProductStatus()};
-        return jdbcTemplate.update(sql,values) > 0;
+        String sql = "insert into product(name,price,id_productline,image) values(?,?,?,?)";
+        Object[] values = {product.getName(), product.getPrice(), product.getProductLine().getId(), product.getImage()};
+        return jdbcTemplate.update(sql, values) > 0;
     }
 
     @Override
     public boolean update(Product product) {
-        String sql = "update product set name=?,inventory=?,price =?,id_productline =?,image=?,status=? where id=?";
-        Object [] values={product.getName(),product.getInventory(),product.getPrice(),product.getProductLine().getId(),product.getImage(),product.getProductStatus(),product.getId()};
-        return jdbcTemplate.update(sql,values) >0 ;
+        String sql = "update product set name=?,inventory=?,price =?,id_productline =?,image=?,`status`=? where id=?";
+        Object[] values = {product.getName(), product.getInventory(), product.getPrice(), product.getProductLine().getId(), product.getImage(), product.getProductStatus().toString(), product.getId()};
+        return jdbcTemplate.update(sql, values) > 0;
     }
 
     @Override
     public boolean delete(Long id) {
-        String sql= "delete from product where id =?";
-        Object[] values={id};
-        return jdbcTemplate.update(sql,values) >0;
+        String sql = "delete from product where id =?";
+        Object[] values = {id};
+        return jdbcTemplate.update(sql, values) > 0;
     }
 }
